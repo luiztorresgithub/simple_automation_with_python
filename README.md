0c06349971f290c7f383bd80d1aee1cbf3a761d5
Luiz Felipe Torres Santos <94366321+luiztorresgithub@users.noreply.github.com>
2025-02-15 19:42:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple_automation_with_python
+ algorithm that automates google searches using python, selenium and chromedriver
---
+++ automation/main.py
@@ -0,0 +1,57 @@
+#type: ignore
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)  # type: ignore
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if __name__ == '__main__': #type: ignore
+    TIME_TO_WAIT = 10
+
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    browser.get('https://www.google.com')
+
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+
+    search_input.send_keys('hello world!')
+    search_input.send_keys(Keys.ENTER)
+
+
+sleep(TIME_TO_WAIT)
